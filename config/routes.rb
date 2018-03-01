d3e5158bbafc7e721f29d7352270e3da2588efcb
jiaowagn <wangjiaohere@gmail.com>
2018-03-01 18:02:59 +0800

    add about page via topics#about

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'about' => 'about#index'
 
+  get 'about2' => "topics#about"
+
   root 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
