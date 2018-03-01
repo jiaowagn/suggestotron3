f9e0482dd08792d370f3bff20d2fc0b978a10cea
jiaowagn <wangjiaohere@gmail.com>
2018-03-01 18:01:11 +0800

    add about page via about controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'about' => 'about#index'
+
   root 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
